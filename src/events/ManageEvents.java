4f5718e8ce75160dc43c7e316ef588517b016795
sethoberg <sethoberg@seths-air.clients.internal.mah.se>
2019-05-09 20:24:40 +0200

    Nya metoder

--- src/events/ManageEvents.java
+++ src/events/ManageEvents.java
@@ -3,6 +3,7 @@ package events;
 import player.Player;
 import player.PlayerList;
 import tiles.Property;
+import tiles.Tax;
 import tiles.Tile;
 
 /**
@@ -25,27 +26,69 @@ public class ManageEvents {
 			propertyEvent(tile, player);
 		}
 		
+		if(tile instanceof Tax) {
+			taxEvent(tile, player);
+		}
+		
 	}
 	
 	
-	//Om spelare inte har råd att betala, eliminera spelare
-	public void control() {
+	/**
+	 * This method is supposed to be called from ANY class that requires the current player to pay any amount,
+	 * if the user does not have said amount they are to be eliminated immediately 
+	 */
+	public void control(Player player, int amount) {
+		
+		if(player.getBalance() < amount) {
+//			playerList.eliminatePlayer(int index); 
+			//In playerList.eliminatePlayer call isAlive == false;
+		}
 		
 	}
 	
 	
 	public void propertyEvent(Tile tile, Player player) {
 		Property tempProperty = (Property) tile; 
+		int tempInt = 0; 
+		
 		
 		//Boolean isOwned should be added to property
 //		if(tempProperty.isOwned == true) {
-			player.setBalance(player.getBalance() - tempProperty.getDefaultRent());
+		    tempInt = player.getBalance() - tempProperty.getTotalRent();
+			player.setBalance(player.getBalance() - tempProperty.getTotalRent());
+//			propertyOwner.addMoney(tempProperty.getTotalRent());
 //		}
 		
 		
-		
-		control();
+//		if(tempProperty.isOwned == false) {
+			/*
+			 * At this point the current player will get a pop-up menu asking if the user wants to purchase a property
+			 * (if they have sufficent funds). The menu will say something like:
+			 * This property is avaliable, do you want to purchase it? with the option of either yes or no
+			 * In the future an auction function could be added in this step as well 
+			 * 
+			 * A method call to this point could loop something like 
+			 * popUpMenu.purchaseProperty(tile, player);
+			 */
+			
+			
+//		}
+			
+//		control();
 	}
+	
+	public void taxEvent(Tile tile, Player player) {
+		Tax tempTaxObject = (Tax) tile;
+		int chargePlayer = tempTaxObject.getTax();
+		
+		control(player, chargePlayer);
+		
+		if(player.isAlive()) {
+			
+		}
+		
+	}
+	
 	
 	
 	
