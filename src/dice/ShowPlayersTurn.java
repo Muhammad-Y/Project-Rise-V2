7b47849e07b2f418b40dd318e200be301531dee8
Rohan <rohan_samandari@hotmail.com>
2019-05-10 14:40:26 +0200

    New

---
+++ src/dice/ShowPlayersTurn.java
@@ -0,0 +1,47 @@
+package dice;
+/**
+ * A Lable which shows current player's turn
+ * @author Rohan
+ */
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.*;
+
+public class ShowPlayersTurn extends JPanel{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String playerName;
+	private JLabel lblPlayer;
+	private Color playerColor;
+	
+	public ShowPlayersTurn(String playerName, Color playerCol) {
+		this.playerName = playerName;
+		this.playerColor = playerCol;
+		showTheLabel();
+	}
+	
+	private void showTheLabel() {
+		setPreferredSize(new Dimension(100,50));
+		setBackground(playerColor);
+		
+		lblPlayer = new JLabel(playerName + "'s turn");
+		lblPlayer.setForeground(Color.white);
+		lblPlayer.setPreferredSize(new Dimension(100,25));
+		add(lblPlayer);
+	}
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setPreferredSize(new Dimension(220, 100));
+		frame.add(new ShowPlayersTurn("Rohan", Color.green));
+		frame.pack();
+		frame.setVisible(true);
+		
+		
+		
+//		new ShowPlayersTurn("Rohan", Color.green);
+	}
+}
