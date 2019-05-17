1bf7094b94ae8d34515c836bb52070d5fe6f1d5b
aevan <aevan@192.168.0.16>
2019-05-17 13:35:36 +0200

    Yaaaa yeet, end screen stuff done

---
+++ src/endGame/WinPanel.java
@@ -0,0 +1,40 @@
+package endGame;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class WinPanel extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private ImageIcon imgBackground = new ImageIcon("images/win.png");
+	private JLabel lblBackground = new JLabel("", imgBackground, JLabel.CENTER);
+
+	public WinPanel() {
+		
+		createFrame();
+
+		lblBackground.setBounds(0, 0, imgBackground.getIconWidth(), imgBackground.getIconHeight());
+
+		
+		add(lblBackground);
+		setVisible(true);
+
+	}
+
+	public void createFrame() {
+		setSize(imgBackground.getIconWidth(), imgBackground.getIconHeight());
+		setLayout(null);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+	}
+	
+	public static void main(String[] args) {
+		WinPanel wp = new WinPanel();
+		
+	}
+
+
+
+}
