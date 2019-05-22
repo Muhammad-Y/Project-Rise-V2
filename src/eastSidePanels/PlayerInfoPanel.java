8fc390ac59ad108c2ee7210e0121213434186fce
Sebastian <sebastian.viro@gmail.com>
2019-05-22 21:21:28 +0200

    Klass korrigerad efter kodgranskning

--- src/eastSidePanels/PlayerInfoPanel.java
+++ src/eastSidePanels/PlayerInfoPanel.java
@@ -13,130 +13,119 @@ import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
+import javax.swing.border.Border;
 
 import player.PlayerList;
 
 /**
+ * The class displays information about the players in a panel.
  * @author Abdulkhuder Muhammad, Sebastian Viro, Rohan Samandari
- *
  */
 public class PlayerInfoPanel extends JPanel {
 
-	// private PlayerList playerList;
-
 	private JLabel lblName = new JLabel("");
 	private JLabel lblRank = new JLabel("");
-	private JLabel lblGold = new JLabel("100G");
-	private JLabel lblNetworth = new JLabel("1000G");
+	private JLabel lblGold = new JLabel("");
+	private JLabel lblNetworth = new JLabel("");
 	private JLabel lblProperty = new JLabel();
-	private JLabel lblISAlive = new JLabel();
+	private JLabel lblIsAlive = new JLabel();
+	
 	private JTextArea lblPropertyList = new JTextArea("");
-	private JScrollPane scroller = new JScrollPane(lblPropertyList);
-//    private String name ;
-	private JPanel p1 = new JPanel();
-	private JPanel p2 = new JPanel();
-	private JPanel p3 = new JPanel();
-	private JPanel p4 = new JPanel();
-	private JPanel p5 = new JPanel();
-	private JPanel p6 = new JPanel();
-	private JPanel p7 = new JPanel();
+
+	private JPanel pnl1 = new JPanel();
+	private JPanel pnl2 = new JPanel();
+	private JPanel pnl3 = new JPanel();
+	private JPanel pnl4 = new JPanel();
+	private JPanel pnl5 = new JPanel();
+	private JPanel pnl6 = new JPanel();
+	private JPanel pnl7 = new JPanel();
 	
 	private Font font = new Font("ALGERIAN", Font.PLAIN, 18);
+	private Border border = BorderFactory.createLineBorder(Color.black);
 
-	public PlayerInfoPanel(PlayerList playerList, int playa) {
+
+	/**
+	 * The constructor draws an information panel with information about a player.
+	 * @param playerList the list of player
+	 * @param player, a players index
+	 */
+	public PlayerInfoPanel(PlayerList playerList, int player) {
 
 		setPreferredSize(new Dimension(350, 715));
 		setBackground(Color.DARK_GRAY);
 		setLayout(null);
 
-		p1.setBounds(10, 5, 330, 50);
-		p2.setBounds(10, 55, 330, 50);
-		p3.setBounds(10, 105, 330, 50);
-		p4.setBounds(10, 155, 330, 50);
-		p5.setBounds(10, 205, 330, 50);
-		p6.setBounds(10, 255, 330, 50);
-		p7.setBounds(10, 305, 330, 600);
+		pnl1.setBounds(10, 5, 330, 50);
+		pnl2.setBounds(10, 55, 330, 50);
+		pnl3.setBounds(10, 105, 330, 50);
+		pnl4.setBounds(10, 155, 330, 50);
+		pnl5.setBounds(10, 205, 330, 50);
+		pnl6.setBounds(10, 255, 330, 50);
+		pnl7.setBounds(10, 305, 330, 600);
 
-		// border shiet
-
-		p1.setBorder(BorderFactory.createLineBorder(Color.black));
-		p2.setBorder(BorderFactory.createLineBorder(Color.black));
-		p3.setBorder(BorderFactory.createLineBorder(Color.black));
-		p4.setBorder(BorderFactory.createLineBorder(Color.black));
-		p5.setBorder(BorderFactory.createLineBorder(Color.black));
-		p6.setBorder(BorderFactory.createLineBorder(Color.black));
-		p7.setBorder(BorderFactory.createLineBorder(Color.black));
+		pnl1.setBorder(border);
+		pnl2.setBorder(border);
+		pnl3.setBorder(border);
+		pnl4.setBorder(border);
+		pnl5.setBorder(border);
+		pnl6.setBorder(border);
+		pnl7.setBorder(border);
 		
-
-		lblName.setText( playerList.getPlayerFromIndex(playa).getName().toUpperCase());
+		lblName.setText( playerList.getPlayerFromIndex(player).getName().toUpperCase());
 		lblName.setFont(font);
 		lblName.setHorizontalAlignment(SwingConstants.CENTER);
 		lblName.setOpaque(false);
 		lblName.setForeground(Color.black);
 		lblName.setBackground(Color.white);
 
-		p1.add(lblName);
-		add(p1);
+		pnl1.add(lblName);
+		add(pnl1);
 
-		lblGold.setText("Amount of gold: " + playerList.getPlayerFromIndex(playa).getBalance());
+		lblGold.setText("Amount of gold: " + playerList.getPlayerFromIndex(player).getBalance());
 		lblGold.setFont(font);
 		lblGold.setHorizontalAlignment(SwingConstants.CENTER);
 		lblGold.setForeground(Color.black);
 
-		p2.add(lblGold);
-		add(p2);
+		pnl2.add(lblGold);
+		add(pnl2);
 
-		lblNetworth.setText("Total wealth: " + playerList.getPlayerFromIndex(playa).getNetWorth());
+		lblNetworth.setText("Total wealth: " + playerList.getPlayerFromIndex(player).getNetWorth());
 		lblNetworth.setFont(font);
 		lblNetworth.setHorizontalAlignment(SwingConstants.CENTER);
-		p3.add(lblNetworth);
-		add(p3);
+		pnl3.add(lblNetworth);
+		add(pnl3);
 
-		lblISAlive.setText(playerList.getPlayerFromIndex(playa).isAliveString());
-		lblISAlive.setFont(font);
-		lblISAlive.setHorizontalAlignment(SwingConstants.CENTER);
-		p4.add(lblISAlive);
-		add(p4);
+		lblIsAlive.setText(playerList.getPlayerFromIndex(player).isAliveString());
+		lblIsAlive.setFont(font);
+		lblIsAlive.setHorizontalAlignment(SwingConstants.CENTER);
+		pnl4.add(lblIsAlive);
+		add(pnl4);
 
-		lblRank.setText("Player Rank: " + playerList.getPlayerFromIndex(playa).getPlayerRank());
+		lblRank.setText("Player Rank: " + playerList.getPlayerFromIndex(player).getPlayerRank());
 		lblRank.setFont(font);
 		lblRank.setHorizontalAlignment(SwingConstants.CENTER);
-		p5.add(lblRank);
-		add(p5);
+		pnl5.add(lblRank);
+		add(pnl5);
 
-		
-		
-		
 		lblProperty.setText("Properties");
 		lblProperty.setFont(font);
 		lblProperty.setHorizontalAlignment(SwingConstants.CENTER);
-		p6.add(lblProperty);
-		add(p6);
+		pnl6.add(lblProperty);
+		add(pnl6);
 
-		for (int i = 0; i < playerList.getPlayerFromIndex(playa).getProperties().size(); i++) {
-
-			lblPropertyList.append(playerList.getPlayerFromIndex(playa).getProperty(i).getName() + "\n");
-
+		/**
+		 * For-loop iterates through all properties a player owns and displays them in the label.
+		 */
+		for (int i = 0; i < playerList.getPlayerFromIndex(player).getProperties().size(); i++) {
+			lblPropertyList.append(playerList.getPlayerFromIndex(player).getProperty(i).getName() + "\n");
 		}
+		
 		lblPropertyList.setFont(font);
 		lblPropertyList.setOpaque(false);
 
-    	p7.add(lblPropertyList);    	
-		add(p7);
+    	pnl7.add(lblPropertyList);    	
+		add(pnl7);
 
 	}
 
-//	public static void main(String[] args) {
-//		PlayerInfoPanel ui = new PlayerInfoPanel();
-//		JFrame frame = new JFrame();
-////		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-////		frame.setUndecorated(true);
-//		frame.setVisible(true);
-//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		frame.add(ui);
-//		frame.pack();
-//		frame.setVisible(true);
-//		frame.setLocationRelativeTo(null);
-//
-//	}
 }
