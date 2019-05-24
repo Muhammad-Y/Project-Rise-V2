f4e28d5364e5802cabbbaf92d2b0f241d5e3ee6b
Rohan <rohan_samandari@hotmail.com>
2019-05-24 10:39:39 +0200

    ok

--- src/events/ManageEvents.java
+++ src/events/ManageEvents.java
@@ -435,7 +435,7 @@ public class ManageEvents {
 			try {
 			
 				for (int i = 0; i < 5; i++) {
-					File musicPath = new File("music/Doro.wav");				
+					File musicPath = new File("music/£££.wav");				
 					AudioInputStream ais = AudioSystem.getAudioInputStream(musicPath);
 					clip = AudioSystem.getClip();
 					clip.open(ais);
